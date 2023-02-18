8b8f2062cc16cd7bc070a6174325e04b1d1d2709
mariaruizgomez <crg.7886@gmail.com>
2023-02-18 04:04:17 -0800

    added loaded function to have a word displayed by default

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,50 +3,67 @@ import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
 
-
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    
+
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSumit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+function load () {
+    setLoaded(true);
+    search();
+}
 
-  return (
-    <div className="Dictionary">
-      <h1>Dictionary</h1>
-
-      <form
-        className="col-6 d-flex me-5 justify-content-center"
-        onSubmit={search}
-      >
-        <input
-          type="search"
-          placeholder="Type a word"
-          autoFocus="on"
-          className="form-control me-3"
-          onChange={handleKeywordChange}
-        />
-        <div>
-          <button className="btn btn-secondary" title="Search">
-            Search
-          </button>
-        </div>
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <h1>Dictionary</h1>
+        <section>
+          <form
+            className="col-12 d-flex me-5 justify-content-center"
+            onSubmit={handleSumit}
+          >
+            <input
+              type="search"
+              placeholder="Type a word"
+              autoFocus="on"
+              className="form-control me-3"
+              onChange={handleKeywordChange}
+            />
+            <div>
+              <button className="btn" title="Search">
+                Search
+              </button>
+            </div>
+          </form>
+          <div className="suggestions">
+            <i class="fa-regular fa-lightbulb"></i>{" "}
+            <strong>most-searched-for definitions: </strong>Trove, Sauté, canny,
+            foray...{" "}
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading"
+  }
 }
